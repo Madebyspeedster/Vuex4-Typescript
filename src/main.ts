45d1022c2009bbc42618759460aba40764cabd17
ng_007 <ypetri@softserveinc.com>
2021-10-16 18:03:10 +0300

    test equal css framework

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import { key, store } from "@/store";
+import Equal from "equal-vue";
+import "equal-vue/dist/style.css";
 
-createApp(App).use(store, key).use(router).mount("#app");
+createApp(App).use(store, key).use(router).use(Equal).mount("#app");
--- src/views/Users.vue
+++ src/views/Users.vue
@@ -4,21 +4,23 @@ import { defineComponent } from "vue";
 import { getUsersList, GithubUser } from "@/api/github";
 import { mapGetters } from "vuex";
 import { ACCESS_USER_KEYS, USER_KEYS } from "@/store/modules/user/types";
+import { Button } from "equal-vue";
 
 export default defineComponent({
+  components: {
+    Button,
+  },
   props: {
     title: {
       type: String,
       default: "Users title",
     },
   },
-  data: function () {
-    return {
-      store: useStore(),
-      test: "Hello",
-      users: [] as GithubUser[],
-    };
-  },
+  data: () => ({
+    store: useStore(),
+    test: "Hello",
+    users: [] as GithubUser[],
+  }),
 
   computed: {
     ...mapGetters({
@@ -40,20 +42,30 @@ export default defineComponent({
   <p>{{ title }}</p>
   <h1>Users Page</h1>
   <h1>{{ test }}</h1>
-
-  <button @click="fetchUsers">Re-fetch</button>
-  <ul style="max-width: 800px; margin: 0 auto" v-if="users.length">
-    <li
-      style="list-style: none; display: inline-block"
-      v-for="gitUSer in users"
-      :key="gitUSer.id"
-    >
-      {{ gitUSer.login }}
-      <img
-        style="width: 100px; height: 100px"
-        :src="gitUSer.avatar_url"
-        alt="{{gitUSer.login}}"
-      />
-    </li>
-  </ul>
+  <section id="main">
+    <Button type="primary" @click="fetchUsers">Fetch Users!</Button>
+    <ul style="max-width: 800px; margin: 0 auto" v-if="users.length">
+      <li
+        style="list-style: none; display: inline-block"
+        v-for="gitUSer in users"
+        :key="gitUSer.id"
+      >
+        {{ gitUSer.login }}
+        <img
+          style="width: 100px; height: 100px"
+          :src="gitUSer.avatar_url"
+          alt="{{gitUSer.login}}"
+        />
+      </li>
+    </ul>
+  </section>
 </template>
+
+<style scoped>
+#main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
